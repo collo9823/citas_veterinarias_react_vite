f2c138201de9ba31b7af71e85754d7141b365855
collo9823 <collo9823@gmail.com>
2022-11-07 21:18:54 -0300

    commiter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header=()=>{
 
-    
+    console.log('dif')
     return(
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto" >Seguimiento Pacientes 
